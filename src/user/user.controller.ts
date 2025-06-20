5d90b8c5cb846c839a8881d3ded1a0baed362fbf
K1dou <hique1276@gmail.com>
2025-06-20 19:44:43 -0300

    feat: implement user signup, deletion, and service methods for user management

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Prisma, User as UserModel } from '@prisma/client';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('signup')
+  async signupUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+
+  @Delete('delete')
+  async deleteUser(
+    @Body() userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<UserModel> {
+    return this.userService.deleteUser(userWhereUniqueInput);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
@@ -9,5 +9,29 @@ export class UserService {
 
   async createUser(data: Prisma.UserCreateInput) {
     return this.prismaService.user.create({ data });
+  }
+  async user(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: userWhereUniqueInput,
+    });
+  }
+
+  async updateUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+  }): Promise<User> {
+    const { where, data } = params;
+    return this.prismaService.user.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prismaService.user.delete({
+      where,
+    });
   }
 }
