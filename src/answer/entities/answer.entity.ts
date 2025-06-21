a424822ce5f6a467f59814e041e87b6609091fbd
K1dou <hique1276@gmail.com>
2025-06-21 15:56:46 -0300

    feat: implement Answer and Question entities with Prisma integration; add User entity

--- src/answer/entities/answer.entity.ts
+++ src/answer/entities/answer.entity.ts
@@ -1,1 +1,10 @@
-export class Answer {}
+import { Answer as AnswerPrisma } from '@prisma/client';
+
+export class Answer implements AnswerPrisma {
+  id: string;
+  body: string;
+  createdAt: Date;
+  updatedAt: Date;
+  questionId: string;
+  userId: number;
+}
--- src/questions/entities/question.entity.ts
+++ src/questions/entities/question.entity.ts
@@ -1,1 +1,10 @@
-export class Question {}
+import { Question as PrismaQuestion } from '@prisma/client';
+
+export class Question implements PrismaQuestion {
+  id: string;
+  title: string;
+  body: string;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: number;
+}
