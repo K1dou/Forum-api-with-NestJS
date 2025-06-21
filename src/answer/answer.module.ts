b524a43658e42b360b7fccc99d750f70487df5cc
K1dou <hique1276@gmail.com>
2025-06-21 16:02:27 -0300

    feat: update Answer and Questions modules to import DatabaseModule instead of PrismaService

--- src/answer/answer.module.ts
+++ src/answer/answer.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AnswerService } from './answer.service';
+import { DatabaseModule } from 'src/database/database.module';
 import { AnswerController } from './answer.controller';
+import { AnswerService } from './answer.service';
 
 @Module({
   controllers: [AnswerController],
   providers: [AnswerService],
+  imports: [DatabaseModule],
 })
 export class AnswerModule {}
--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
 import { PrismaService } from 'src/database/prisma.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   controllers: [QuestionsController],
   providers: [QuestionsService],
-  imports: [PrismaService],
+  imports: [DatabaseModule],
 })
 export class QuestionsModule {}
